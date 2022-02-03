0ad35843affbcbbce75daa6db3ab5044bb59be03
Sunjae95 <tjswo9502@gmail.com>
2022-02-03 16:32:30 +0900

    Feat: IconButton 컴포넌트 구현

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,3 @@
 export { default as BadgeImage } from './badge-image.png';
+export { default as CloseImage } from './close-image.png';
+export { default as OpenImage } from './open-image.png';
---
+++ src/components/IconButton.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import { CloseImage, OpenImage } from 'assets';
+
+const IconButton = ({ direction, isClicked }) => (
+  <TagContainer direction={direction}>
+    <img
+      src={isClicked ? OpenImage : CloseImage}
+      alt=""
+      width={32}
+      height={32}
+    />
+  </TagContainer>
+);
+
+IconButton.propTypes = {
+  direction: PropTypes.shape({
+    top: PropTypes.number.isRequired,
+    bottom: PropTypes.number.isRequired,
+    right: PropTypes.number.isRequired,
+    left: PropTypes.number.isRequired,
+  }).isRequired,
+  isClicked: PropTypes.bool.isRequired,
+};
+
+const TagContainer = styled.div`
+  width: 40px;
+  height: 40px;
+
+  position: absolute;
+  ${({ direction }) => css`
+    top: ${direction.top}px;
+    bottom: ${direction.bottom}px;
+    right: ${direction.right}px;
+    left: ${direction.left}px;
+  `}
+`;
+
+export default IconButton;
