f93f068e7e4345e8a584f9a19e548ecf40251947
Sunjae95 <tjswo9502@gmail.com>
2022-02-03 16:04:36 +0900

    Feat: 뱃지 컴포넌트 구현

---
+++ src/components/Badge.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { BadgeImage } from 'assets';
+import PropTypes from 'prop-types';
+
+const Badge = ({ number }) => (
+  <Container>
+    {number}
+    <Percent>%</Percent>
+  </Container>
+);
+
+Badge.proptype = {
+  number: PropTypes.number.isRequired,
+};
+
+const Container = styled.div`
+  font-size: 11px;
+  font-weight: 700;
+  line-height: 25px;
+  text-align: center;
+  word-spacing: 0px;
+  color: #ffffff;
+
+  background-image: url(${BadgeImage});
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+  background-size: contain;
+
+  width: 24px;
+  height: 28px;
+  position: absolute;
+  top: 0px;
+  bottom: 76px;
+  right: 5px;
+  left: 75px;
+
+  cursor: pointer;
+`;
+
+const Percent = styled.span`
+  font-size: 10px;
+  font-weight: 700;
+  line-height: 25px;
+  text-align: center;
+  word-spacing: 0px;
+`;
+
+export default Badge;
